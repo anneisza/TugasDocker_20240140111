ee1298c4b0a4a8ec193f16d95bd421c83d902249
anneisza <rizaanneisza@gmail.com>
2026-04-14 10:16:12 +0700

    Memperbaiki pom.xml

--- src/main/java/com/tugas/deploy/controller/UserController.java
+++ src/main/java/com/tugas/deploy/controller/UserController.java
@@ -1,4 +1,8 @@
 package com.tugas.deploy.controller;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
 public class UserController {
+
 }
--- src/main/java/com/tugas/deploy/model/User.java
+++ src/main/java/com/tugas/deploy/model/User.java
@@ -1,4 +1,14 @@
 package com.tugas.deploy.model;
 
+import.lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    private String nama;
+    private String nim;
+    private String jenisKelamin;
 }
